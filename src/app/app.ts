8c55b99d0a4783ae17c8f99dd18793a68ca48d96
Nealrovi <nealrovi@gmail.com>
2025-11-16 20:12:30 -0500

    Mueve componentes a carpeta shared.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
-import { Header } from "src/header/header";
-import { Descriptor } from "src/descriptor/descriptor";
-import { NavBar } from "src/nav-bar/nav-bar";
+import { Header } from "src/app/shared/header/header";
+import { Descriptor } from "src/app/shared/descriptor/descriptor";
+import { NavBar } from "src/app/shared/nav-bar/nav-bar";
 
 @Component({
   selector: 'app-root',
