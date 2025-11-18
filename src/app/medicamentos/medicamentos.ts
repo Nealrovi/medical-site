5394b19a4ae0ed17109bf95febb9bb7eedf89e48
Nealrovi <nealrovi@gmail.com>
2025-11-18 18:51:11 -0500

    Agrega imagenes y tarjetas de medicamentos.

--- src/app/medicamentos/medicamentos.ts
+++ src/app/medicamentos/medicamentos.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-medicamentos',
-  imports: [],
-  template: `
-    <p>
-      medicamentos works!
-    </p>
-  `,
-  styles: ``
+  imports: [MatCardModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: "./medicamentos.html",
+  styleUrls: ['./medicamentos.css'],
 })
 export class Medicamentos {
 
