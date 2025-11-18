236224a65ba12bb8ef502444a18214578e8f4e38
Leonardo <criletera@gmail.com>
2025-11-18 16:30:15 -0500

    pestaña de medicamentos

--- src/app/shared/nav-bar/nav-bar.ts
+++ src/app/shared/nav-bar/nav-bar.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
-  selector: 'app-nav-bar',
-  imports: [RouterLink],
+  selector: "app-nav-bar",
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: "nav-bar.html",
-  styleUrls: ['nav-bar.css'],
+  styleUrls: ["nav-bar.css"],
 })
-export class NavBar {
-
-}
+export class NavBar {}
