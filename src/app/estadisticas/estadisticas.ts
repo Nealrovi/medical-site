c26b10e4a873d0f68e68623f54b6062f938c388b
Leonardo <criletera@gmail.com>
2025-11-28 21:26:31 -0500

    feat: Agrega información en página de estadísticas.

--- src/app/estadisticas/estadisticas.ts
+++ src/app/estadisticas/estadisticas.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CanvasJSAngularChartsModule } from "@canvasjs/angular-charts";
 
 @Component({
-  selector: 'app-estadisticas',
-  imports: [],
- templateUrl: 'estadisticas.html',
-  styleUrls: [`estadisticas.css`]
+  selector: "app-estadisticas",
+  imports: [CanvasJSAngularChartsModule],
+  templateUrl: "estadisticas.html",
+  styleUrls: [`estadisticas.css`],
 })
 export class Estadisticas {
-
+  chartOptions = {
+    animationEnabled: true,
+    title: {
+      text: "Inventario por medicamento",
+    },
+    data: [
+      {
+        type: "doughnut",
+        yValueFormatString: "#,###.##'%'",
+        indexLabel: "{name}",
+        dataPoints: [
+          { y: 28, name: "Acetaminofem niños" },
+          { y: 10, name: "Acetato de aluminio" },
+          { y: 20, name: "Aciclovir" },
+          { y: 15, name: "Amoxicilina" },
+          { y: 23, name: "Dolex gripa" },
+          { y: 17, name: "Esomeprazol" },
+        ],
+      },
+    ],
+  };
 }
